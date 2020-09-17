5d2437800c7e172839a9632839b61e941e7b5ff4
Daichi Yoshikawa <yossi@ubi.cs.ritsumei.ac.jp>
2020-09-17 15:58:33 +0900

    動かん(スクレイピング)

--- Bowling3/BusinessHoursViewController.swift
+++ Bowling3/BusinessHoursViewController.swift
@@ -22,42 +22,58 @@ class BusinessHoursViewController: UIViewController, UITableViewDataSource, UITa
         super.viewDidLoad()
         
         tableView.delegate = self
+        tableView.dataSource = self
         getBusinessHours()
         // Do any additional setup after loading the view.
     }
     
     func getBusinessHours(){
+        
+//        AF.request("https://www.round1.co.jp/shop/tenpo/shiga-hamaotsu.html").responseString{
+//            response in
+//
+//            print("\(response)")
+//        }
         //スクレイピング対象のサイトを指定
-        Alamofire.request("https://www.round1.co.jp/shop/tenpo/shiga-hamaotsu.html").responseString{
-        response in
-            if let html = response.result.value{
-                if let html = doc = try? HTML(html, encoding: .utf8){
+        AF.request("https://www.round1.co.jp/shop/tenpo/shiga-hamaotsu.html").responseString{
+            response in
+            if let html = response.value{
+                if let doc = try? HTML(html: html, encoding: .utf8){
+                    print("動いてる")
+
                     
+                    let xxx = doc.xpath("//table[@class='table_inline']")
+                    print("\(xxx)")
                     //営業時間のリストのタイトルを取得
                     var businessHourTitle = [String]()
-                    for link in doc.xpath("//th[@class='table_inline']"){
-                        lists.append(link.text ?? "")
+                    for link in doc.xpath("//table[@class='table_inline']/tbody/tr/th"){
+                        businessHourTitle.append(link.text ?? "")
+                        print("タイトル取れてる")
                     }
                     //営業時間を取得
                     var businessHourRange = [String]()
-                    for link in doc.dpath("//td[@class='table_inline']"){
+//                    for link in doc.xpath("//table[@class='table_inline']/td[0]"){
+                    for link in doc.xpath("//table[@class='table_inline']/tbody/tr/th"){
                         businessHourRange.append(link.text ?? "")
+                        print("営業時間取れてる")
                     }
-                    
+
                     //配列に格納
-                    for (index, value) in sizes.enumerated(){
+                    for (index, value) in businessHourTitle.enumerated(){
                         let businessHour = BusinessHour()
                         businessHour.title = value
                         businessHour.range = businessHourRange[index]
                         self.businesshours.append(businessHour)
+                        print("配列に格納してるよー")
                     }
-                    
+
                     self.tableView.reloadData()
-                    
+
                 }
-        
+
             }
         }
+        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -66,17 +82,21 @@ class BusinessHoursViewController: UIViewController, UITableViewDataSource, UITa
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "businessHours", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "businessHoursCell", for: indexPath)
 
         let business = self.businesshours[indexPath.row]
+        print("business\(business.title)")
         
         //sizeを表示
         let labelTitle = cell.viewWithTag(1) as! UILabel
         labelTitle.text = business.title
         
+        
         //priceを表示
         let labelRange = cell.viewWithTag(2) as! UILabel
         labelRange.text = business.range
+        
+        print("表にしてるよー")
         
         return cell
     }
